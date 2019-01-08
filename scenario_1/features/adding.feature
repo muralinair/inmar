c168f8b9b1d5deeaab03e00fd528c6573c606bce
Murali Krishna Thototta Govindan <mthotottagov@factset.com>
2019-01-08 16:54:52 +0530

    Corrected logic for calculation and added logic for digital verification. 	modified:   adding.feature 	modified:   step_definition/step_code.rb

--- scenario_1/features/adding.feature
+++ scenario_1/features/adding.feature
@@ -1,6 +1,6 @@
 Feature: Adding of two numbers 
     Scenario Outline: Add two numbers
-    Given the input1 "<input1>" and input2 "<input2>"
+    Given the input1 "<input1>" and input2 "<input2>" are digits
     When the calculator is run 
     Then the output should be "<output>"
     Examples:
--- scenario_1/features/step_definition/step_code.rb
+++ scenario_1/features/step_definition/step_code.rb
@@ -1,10 +1,14 @@
-Given(/^the input1 "([^"]*)" and input2 "([^"]*)"$/) do |input1,input2|
+#require 'rspec'
+Given(/^the input1 "([^"]*)" and input2 "([^"]*)" are digits$/) do |input1,input2|
 	@input1 = input1  # First digit for addition
     @input2 = input2  # Second digit for addition
+    expect(@input1).to match(/\A[-+]?[0-9]*\.?[0-9]+\Z/)
+    expect(@input2).to match(/\A[-+]?[0-9]*\.?[0-9]+\Z/)
 end
 When (/^the calculator is run$/) do
-    puts "Sum of #{@input1} + #{@input2} is #{@input1.to_i+@input2.to_i}"
+    @output=@input1.to_i+@input2.to_i
+    puts "\nSum of #{@input1} + #{@input2} is #{@output}"
 end
 Then (/^the output should be "([^"]*)"$/) do |expected_output|
-	expect(@output).equal?(expected_output)
+    expect(@output).to eq(expected_output.to_i)
 end
